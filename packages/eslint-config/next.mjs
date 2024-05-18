0127847c517ce1d231aa2246711d0ec0be97150f
Ibrahim Saberi <ibrahims@tilli.pro>
2024-05-17 20:51:30 -0400

    ci: why wont this publish...

--- packages/eslint-config/next.mjs
+++ packages/eslint-config/next.mjs
@@ -42,33 +42,3 @@ export default [
     },
   },
 ];
-
-// const old = {
-//   extends: [
-//     "eslint:recommended",
-//     "prettier",
-//     require.resolve("@vercel/style-guide/eslint/next"),
-//     "eslint-config-turbo",
-//   ],
-//   globals: {
-//     React: true,
-//     JSX: true,
-//   },
-//   env: {
-//     node: true,
-//   },
-//   plugins: ["only-warn"],
-//   settings: {
-//     "import/resolver": {
-//       typescript: {
-//         project,
-//       },
-//     },
-//   },
-//   ignorePatterns: [
-//     // Ignore dotfiles
-//     ".*.js",
-//     "node_modules/",
-//   ],
-//   overrides: [{ files: ["*.js?(x)", "*.ts?(x)"] }],
-// };
