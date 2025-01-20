57f141c2ababaa5f3ba4640e13f9f6a4bfbed191
Michael Vuolo <michaelv@tilli.pro>
2025-01-19 20:54:43 -0500

    feat: refactor configuration files and update eslint settings

--- apps/docs/eslint.config.mjs
+++ apps/docs/eslint.config.mjs
@@ -1,6 +1,15 @@
-import turboNext from "@tilli-pro/eslint-config/next.mjs";
+import baseConfig from "@tilli-pro/eslint-config/base";
+import nextjsConfig from "@tilli-pro/eslint-config/nextjs";
+import reactConfig from "@tilli-pro/eslint-config/react";
+import storybookConfig from "@tilli-pro/eslint-config/storybook";
 
+/** @type {import('typescript-eslint').Config} */
 export default [
-  ...turboNext,
-  { ignores: [".next/*", "node_modules", ".next"] },
+  {
+    ignores: [".next/**"],
+  },
+  ...baseConfig,
+  ...reactConfig,
+  ...nextjsConfig,
+  ...storybookConfig,
 ];
